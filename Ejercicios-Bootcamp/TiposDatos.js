2ca0fcc51cb73a4467dc06949c873716f0199c47
AlexQuirozJ <85137418+AlexQuirozJ@users.noreply.github.com>
2021-06-16 10:08:38 -0500

    Add files via upload
    tipo de datos y console log con $

---
+++ Ejercicios-Bootcamp/TiposDatos.js
@@ -0,0 +1,15 @@
+var dato1=24;
+var dato2="Holaaa";
+var dato3="false";
+var dato4=5.377843478348392892389848409090943434;
+var dato5=true;
+var dato6=null;
+var dato7="hoy es un lindo día";
+var dato8=829;
+var dato9="null"
+var dato10=false;
+
+var suma = parseInt(dato4) + dato8;
+
+console.log(typed(dato4.toFixed(40)));
+console.log("parseFloat:" )
