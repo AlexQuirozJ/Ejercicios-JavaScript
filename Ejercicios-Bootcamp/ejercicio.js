c764a37526fa89002856e752847a8b4433972b4c
AlexQuirozJ <85137418+AlexQuirozJ@users.noreply.github.com>
2021-06-17 10:14:42 -0500

    Add files via upload
    ejercicio Carlos JS

---
+++ Ejercicios-Bootcamp/ejercicio.js
@@ -0,0 +1,33 @@
+/*var valor1 = prompt("ingresa el primer valor", "");
+var valor2 = prompt("ingresa el segundo valor", "");
+if( valor1<valor2 )
+{
+    alert("el valor 1 es mas pequeño");
+}
+else
+{
+    alert("el valor 1 es mayor");
+}*/
+
+/* var valor3 = prompt("ingresa el primer valor", "");
+var valor4 = prompt("ingresa el segundo valor", "");
+if( valor3 > valor4 )
+{
+    alert("el valor 1 es mas grande");
+}
+else
+{
+    alert("el valor 1 es menor ");
+}*/
+
+var valor6 = prompt("ingresa el primer valor", "");
+var valor7 = prompt("ingresa el segundo valor", "");
+if( valor6 == valor7 )
+{
+    alert("el valor es igual");
+}
+else
+{
+    alert("el valor es diferente ");
+}
+
